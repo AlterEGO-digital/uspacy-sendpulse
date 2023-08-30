468f5bb069ce5fea5882876130b3a2014cf188ce
MysikM <mishamysik@gmail.com>
2023-08-30 12:52:08 +0300

    refactore

---
+++ src/const.ts
@@ -0,0 +1,1 @@
+export const SENDPULSE_API = 'https://test-sendpulse.alterego.biz.ua/sendpulse/v1';
--- src/store/reducers/auth/action.ts
+++ src/store/reducers/auth/action.ts
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+import { SENDPULSE_API } from '../../../const';
 import { getToken } from '../../../helpers/db';
 
 export const getTokenInfo = createAsyncThunk('apps/getTokenInfo', async (_, thunkAPI) => {
 	try {
 		const token = await getToken();
-		return await fetch('https://test-sendpulse.alterego.biz.ua/sendpulse/v1/tokens/getTokenInfo', {
+		return await fetch(`${SENDPULSE_API}/tokens/getTokenInfo`, {
 			headers: { Accept: 'application/json', 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
 		}).then(async (res) => {
 			return await res.json();
@@ -18,7 +19,7 @@ export const getTokenInfo = createAsyncThunk('apps/getTokenInfo', async (_, thun
 export const addToken = createAsyncThunk('apps/addToken', async (data: { client_id: string; client_secret: string }, thunkAPI) => {
 	try {
 		const token = await getToken();
-		return await fetch('https://test-sendpulse.alterego.biz.ua/sendpulse/v1/tokens/addToken', {
+		return await fetch(`${SENDPULSE_API}/tokens/addToken`, {
 			method: 'POST',
 			body: JSON.stringify(data),
 			headers: { Accept: 'application/json', 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
