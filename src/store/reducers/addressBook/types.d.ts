287216061ce9714075fd6c09f3f6b2771a19f92e
dprokoptsov <dprokoptsov.codica@gmail.com>
2024-11-11 14:05:26 +0200

    feat: extend store with new functionality of export status

--- src/store/reducers/addressBook/types.d.ts
+++ src/store/reducers/addressBook/types.d.ts
@@ -1,14 +1,17 @@
 import { IAddressBook } from '../../../models/IAddressBook';
 import { IAddressbookForEntity } from '../../../models/IAddressbookForEntity';
 import { IDealsStatus } from '../../../models/IDealsStatus';
+import { IExportStatus } from '../../../models/IExportStatus';
 
 export interface IState {
 	addressBooks: IAddressBook[];
 	addressBooksForEntity: IAddressbookForEntity;
 	dealsStatus: IDealsStatus;
+	exportStatus: IExportStatus;
 	loadingAddressBooks: boolean;
 	loadingAddressBooksForEntity: boolean;
 	loadingUpdateAddressBooksForEntity: boolean;
 	loadingDealStatus: boolean;
+	loadingExportStatus: boolean;
 	errorMessage: string;
 }
